bfe092c6858a8b8b58bdffcbc40eaab687a9b743
Arshiya Vyas <arshiya.vyas@gmail.com>
2025-09-12 12:03:42 -0400

    Initial Commit

---
+++ SecureVault/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SecureVault
+//
+//  Created by Arshiya Vyas on 9/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SecureVault/SecureVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureVaultApp.swift
+//  SecureVault
+//
+//  Created by Arshiya Vyas on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SecureVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
